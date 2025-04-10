d4bf6678869891a53458f5b9c9ea8f83c3d7a8f9
Mateus Back <m.back@nextage.com.br>
2025-04-10 19:44:47 -0300

    feat: aula 3

--- paginasweb/views.py
+++ paginasweb/views.py
@@ -5,3 +5,12 @@ from django.views.generic import TemplateView
 # Create your views here.
 class PaginaInicial(TemplateView):
     template_name = 'paginasweb/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PaginaInicial, self).get_context_data(**kwargs)
+        context['titulo'] = 'Pagina Inicial'
+        return context
+
+
+class SobreView(TemplateView):
+    template_name = 'paginasweb/sobre.html'
